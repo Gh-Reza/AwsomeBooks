665fb88ccd82d2ca48ce7675e9cfe01870cbb2ce
Reza Merzaie <ghrezam97@gmail.com>
2023-05-30 11:05:39 +0430

    Bug fixed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const library = document.querySelector('.library');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
+let removeBtns = document.querySelectorAll('.removeButton');
 
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
@@ -20,9 +21,23 @@ function displayLibrary(book) {
     <hr>
   </li>  
   `;
+  removeBtns = document.querySelectorAll('.removeButton');
 }
 
 books.forEach((book) => displayLibrary(book));
+
+function removeBook() {
+  removeBtns.forEach((removeBtn, index) => {
+    removeBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      books = books.filter((book) => book !== books[index]);
+      updateLocalStorage();
+      library.innerHTML = '';
+      books.forEach((book) => displayLibrary(book));
+      removeBook();
+    });
+  });
+}
 
 document.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -33,17 +48,8 @@ document.addEventListener('submit', (e) => {
   updateLocalStorage();
   library.innerHTML = '';
   books.forEach((book) => displayLibrary(book));
+  removeBook();
   e.target.reset();
 });
 
-const removeBtns = document.querySelectorAll('.removeButton');
-
-removeBtns.forEach((removeBtn, index) => {
-  removeBtn.addEventListener('click', (e) => {
-    books = books.filter((book) => book !== books[index]);
-    updateLocalStorage();
-    library.innerHTML = '';
-    books.forEach((book) => displayLibrary(book));
-    e.target.reset();
-  });
-});
+removeBook();
